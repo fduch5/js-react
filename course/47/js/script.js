364514f3d62026cf1c58c00031684ba4a4c3e49f
Дамба Александр Шораанович Д.А.Ш. 664503 <das664503@tuvsu.local>
2025-02-07 11:36:46 +0700

    47 - Рекурсия. Пример 2 с рекурсией

--- course/47/js/script.js
+++ course/47/js/script.js
@@ -22,8 +22,11 @@ const students = {
         }]
     },
 
-    some: {
-        
+    semi: {
+        students: [{
+            name: "Andrey",
+            progress: 100
+        }]
     }
 };
 
@@ -34,6 +37,7 @@ function getTotalProgressByIteration(data){
 
     for(let course of Object.values(data)){
         if(Array.isArray(course)){
+            students += course.length;
             for(let i = 0; i < course.length; i += 1){
                 total += course[i].progress;
             }
@@ -53,3 +57,27 @@ function getTotalProgressByIteration(data){
 
 
 console.log(getTotalProgressByIteration(students));
+
+
+function getTotalProgressByRecursion(data){
+    if(Array.isArray(data)){
+        let total = 0;
+        for(let i = 0; i < data.length; i += 1){
+            total += data[i].progress;
+        }
+        return [total, data.length];
+    } else {
+        let total = [0, 0];
+        for( let subData of Object.values(data)){
+            const subDataArray = getTotalProgressByRecursion(subData);
+            total[0] += subDataArray[0];
+            total[1] += subDataArray[1];
+        }
+
+        return total;
+    }
+}
+
+const total = getTotalProgressByRecursion(students);
+
+console.log(total[0] / total[1]);
